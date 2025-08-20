e642c93e6b3c18880599b86cbdc516019b2ba928
petsereypanha <sereypanhapet@gmail.com>
2025-08-20 23:56:52 +0700

    update basics

---
+++ src/basic/Associations.ts
@@ -0,0 +1,26 @@
+interface Id<T extends string | number> {
+    id: T
+}
+class Author {
+    constructor(
+        private id: string,
+        private name: string,
+    ) {}
+}
+
+class Blog implements Id<string> {
+    id: string
+
+    author: Author
+
+    constructor(id: string, author: Author) {
+        this.id = id
+
+        this.author = author
+    }
+}
+
+export {
+    Author,
+    Blog
+}
