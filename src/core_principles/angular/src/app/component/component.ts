9ca85063c500b9219c025292fa8c77c354f13862
petsereypanha <sereypanhapet@gmail.com>
2025-08-21 21:42:56 +0700

    add core principles angular

---
+++ src/core_principles/angular/src/app/component/component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-component',
+  imports: [],
+  templateUrl: './component.html',
+  styleUrl: './component.scss'
+})
+export class Component {
+
+}
